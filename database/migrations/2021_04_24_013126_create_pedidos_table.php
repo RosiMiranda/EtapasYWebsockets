a17dacc94717401d428726ddb25670893759fc6e
rosmir <36215317+RosiMiranda@users.noreply.github.com>
2021-04-23 20:52:59 -0500

    GUI creation
    Co-Authored-By: AndreaBecerra <andrea.becerrab20@gmail.com>
    Co-Authored-By: liaDiaz <46771468+liaDiaz@users.noreply.github.com>
    Co-Authored-By: Oscar Antonio Becerril López <oskbrrlz@gmail.com>

---
+++ database/migrations/2021_04_24_013126_create_pedidos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePedidosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pedidos', function (Blueprint $table) {
+            $table->id();
+            $titulo->varchar(200);
+            $table->timestamps();
+            $table->unsignedBigInteger('transacciones_id');
+            $table->foreign('transacciones_id')->references('id')->on('transacciones')->onDelete('transacciones');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pedidos');
+    }
+}
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,100 +1,87 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<!DOCTYPE HTML>
+<html>
+  <head>
+          <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <style>
+      #div1, #div2, #div3, #div4{
 
-        <title>Laravel</title>
+        float: left;
+        width: 200px;
+        height: 500px;
+        margin: 25px;s
+        padding: 10px;
+        border: 1px solid black;
+      }
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+      #divh1, #divh2{
+        width: 200px;
+        
+        height: 200px;
+        border: 1px solid black;
+      }
+      h4{
+          padding: 0;
+          margin : 3px;
+          
+      }
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
+      .pedidoCard{
+          background-color: darkcyan;
+          width: 90%;
+          height: 31px;
+          margin : 5px;
+          text-align: center;
+          
+      }
+      .row-width{
+        width: 70%;
+      }
 
-            .full-height {
-                height: 100vh;
-            }
+    </style>
+    <script>
+      function allowDrop(ev) {
+        ev.preventDefault();
+      }
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+      function drag(ev) {
+        ev.dataTransfer.setData("text", ev.target.id);
+      }
 
-            .position-ref {
-                position: relative;
-            }
+      function drop(ev) {
+        ev.preventDefault();
+        var data = ev.dataTransfer.getData("text");
+        ev.target.appendChild(document.getElementById(data));
+      }
+    </script>
+  </head>
+<body>
+  <h2>Dashboard</h2>
+  <div style="padding:30px" class="row justify-content-between row-width"> 
+    <button class="btn-primary"> Crear un pedido </button>
+    <div class="btn-success"> Alertita ahahahahahahahaha :) </div>
+  </div>
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
+  <div>
+      <div id="div1" ondrop="drop(event)" ondragover="allowDrop(event)">
+          <h4>1. Salida de planta</h4>
+          <div draggable="true" ondragstart="drag(event)" id="drag2" class="pedidoCard"> test </div>
+      </div>
 
-            .content {
-                text-align: center;
-            }
+      <div id="div2" ondrop="drop(event)" ondragover="allowDrop(event)">
+          <h4>2. En Local Delivery Center</h4>
+      </div>
 
-            .title {
-                font-size: 84px;
-            }
+      <div id="div3" ondrop="drop(event)" ondragover="allowDrop(event)">
+          <h4>3. En proceso de entrega</h4>
+      </div>
 
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
+      <div id="div4" ondrop="drop(event)" ondragover="allowDrop(event)">
+        <h4>4. Entregado</h4>
+        <div id="divh1"><p>a. Completa</p></div>
+        <div id="divh2"><p>b.Fallida</p></div>
 
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
-        </div>
-    </body>
+      </div>
+  </div>
+  </body>
 </html>
